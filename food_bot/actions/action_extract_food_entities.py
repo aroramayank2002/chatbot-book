32490ef57ae19bea4dcac8f41b49e7a3f9b8f378
Mayank <mayank.arora@dibtravel.com>
2025-07-24 09:42:02 +0200

    Improvement

--- food_bot/actions/action_extract_food_entities.py
+++ food_bot/actions/action_extract_food_entities.py
@@ -9,9 +9,15 @@ class ActionListEntities(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # Extract entities from the user's message
-        food_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "food"]
-        spice_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "spice"]
-        qty_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "qty"]
+        # food_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "food"]
+        # spice_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "spice"]
+        # qty_entities = [e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "qty"]
+
+        # Avoid duplicate in logs unlike the above lines
+        food_entities = list(set([e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "food"]))
+        spice_entities = list(set([e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "spice"]))
+        qty_entities = list(set([e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "qty"]))
+        qty_medium = list(set([e["value"] for e in tracker.latest_message["entities"] if e["entity"] == "medium"]))
 
         # Compile the response message
         response = "Here are the details I extracted:\n"
@@ -22,9 +28,11 @@ class ActionListEntities(Action):
             response += f"- Foods: {', '.join(food_entities)}\n"
         if spice_entities:
             response += f"- Spices: {', '.join(spice_entities)}\n"
+        if qty_medium:
+            response += f"- Medium: {', '.join(spice_entities)}\n"
 
         if not qty_entities and not food_entities and not spice_entities:
-            response += "I couldn't find any information related to food, spices, or quantities."
+            response += "I couldn't find any information related to food, spices, quantities or medium."
 
         # Send the response back to the user
         dispatcher.utter_message(text=response)
